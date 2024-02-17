399515886444763d9917203723e46332529288eb
samAricha <samaricha001@gmail.com>
2024-02-17 12:44:18 +0300

    fetching account and transaction types

--- app/Http/Controllers/AccountTypeController.php
+++ app/Http/Controllers/AccountTypeController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AccountType;
+use App\Models\TransactionType;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AccountTypeController extends Controller
 {
+    use HttpResponses;
+
     public function index()
     {
+        try {
+            $accountTypes =  AccountType::all();
 
+            return $this->success(
+                $accountTypes,
+                'Account Types successfully fetched',
+                Response::HTTP_OK
+            );
+
+
+        } catch (\Exception $e) {
+            return $this->error(
+                $e->getMessage(),
+                'Error fetching Account Types',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     public function show($id)
--- app/Http/Controllers/TransactionTypeController.php
+++ app/Http/Controllers/TransactionTypeController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransactionType;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TransactionTypeController extends Controller
 {
+    use HttpResponses;
+
     public function index()
     {
+        try {
+            $transactionTypes =  TransactionType::all();
 
+            return $this->success(
+                $transactionTypes,
+                'Transaction Types successfully fetched',
+                Response::HTTP_OK
+            );
+
+
+        } catch (\Exception $e) {
+            return $this->error(
+                $e->getMessage(),
+                'Error fetching Transaction Types',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     public function show($id)
